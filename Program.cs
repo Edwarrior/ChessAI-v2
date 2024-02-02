3bdc07fb064bbfaef24cfc7eac2ca07679d55070
Edward Tompkinson <ecptompkinson@hotmail.co.uk>
2024-02-02 06:58:03 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+
+        // Create a Form
+        Form mainForm = new Form
+        {
+            Text = "Image Viewer",
+            Size = new System.Drawing.Size(400, 300)
+        };
+
+
+
+        // Run the application
+        Application.Run(mainForm);
+    }
+}
